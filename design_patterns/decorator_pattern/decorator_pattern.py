728b44adc13d9270c4a64cd1efda3b56483df7c6
M Krishna <krishnamurugan589@gmail.com>
2022-03-07 15:08:53 +0530

    ran black

--- design_patterns/decorator_pattern/decorator_pattern.py
+++ design_patterns/decorator_pattern/decorator_pattern.py
@@ -4,28 +4,30 @@ Decorator Pattern
 
 from abc import ABC, abstractmethod
 
+
 class Beverage(ABC):
 
     description: str = "Unknown description"
 
-    def getDescription(self) -> str: 
+    def getDescription(self) -> str:
         return self.description
 
     @abstractmethod
-    def cost(self) -> float: pass
+    def cost(self) -> float:
+        pass
 
 
 class CondimentDecorator(Beverage, ABC):
 
     beverage: Beverage
-    
+
     @abstractmethod
-    def getDescription(self) -> str: pass
+    def getDescription(self) -> str:
+        pass
 
 
 # Concrete Implementations of the Beverage type
 class HouseBlend(Beverage):
-
     def __init__(self):
         self.description = "HouseBlend"
 
@@ -34,7 +36,6 @@ class HouseBlend(Beverage):
 
 
 class DarkRoast(Beverage):
-
     def __init__(self):
         self.description = "DarkRoast"
 
@@ -44,7 +45,6 @@ class DarkRoast(Beverage):
 
 # Concrete Implementations of the Condiments type
 class Milk(CondimentDecorator):
-
     def __init__(self, beverage: Beverage):
         self.description = "Milk"
         self.beverage = beverage
@@ -55,8 +55,8 @@ class Milk(CondimentDecorator):
     def cost(self) -> float:
         return self.beverage.cost() + 1.1
 
-class Mocha(CondimentDecorator):
 
+class Mocha(CondimentDecorator):
     def __init__(self, beverage: Beverage):
         self.description = "Mocha"
         self.beverage = beverage
@@ -67,17 +67,24 @@ class Mocha(CondimentDecorator):
     def cost(self) -> float:
         return self.beverage.cost() + 2.2
 
+
 # Object creation phase
 house_blend = HouseBlend()
-print("Description", house_blend.getDescription()) # get the description of the Beverage
+print(
+    "Description", house_blend.getDescription()
+)  # get the description of the Beverage
 print("Cost", house_blend.cost())
 
 # create a new condiment object
-house_blend_with_milk_topping = Milk(house_blend) # pass the beverage you want to decorate
+house_blend_with_milk_topping = Milk(
+    house_blend
+)  # pass the beverage you want to decorate
 print(house_blend_with_milk_topping.getDescription())
 
-house_blend_with_milk_and_mocha_toppings = Mocha(house_blend_with_milk_topping) # here we are topping the milk condiment with mocha
+house_blend_with_milk_and_mocha_toppings = Mocha(
+    house_blend_with_milk_topping
+)  # here we are topping the milk condiment with mocha
 print(house_blend_with_milk_and_mocha_toppings.getDescription())
 
 # print the cost of the final decorated object
-print(house_blend_with_milk_and_mocha_toppings.cost()) # print out the final cost
+print(house_blend_with_milk_and_mocha_toppings.cost())  # print out the final cost
